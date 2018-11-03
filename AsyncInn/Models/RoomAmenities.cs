add1dccfac365231cc932a2ab2d8ad618447b5d3
carlos cadena <carloscadena@live.com>
2018-11-03 16:52:54 -0700

    working room details

--- AsyncInn/Models/RoomAmenities.cs
+++ AsyncInn/Models/RoomAmenities.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,9 @@ namespace AsyncInn.Models
 {
     public class RoomAmenities
     {
+        [Display(Name = "Amenity")]
         public int AmenitiesID { get; set; }
+        [Display(Name = "Room Name: ")]
         public int RoomID { get; set; }
 
         public Room Room { get; set; }
